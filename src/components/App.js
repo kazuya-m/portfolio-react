9632dd05f667cd53352dd4e92070f0641521f0b2
kazuya-m <kazuya.matsuo5@gmail.com>
2020-06-11 20:06:32 +0900

    add material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends React.Component{
   render() {
     return (
-      <div>
+      <MuiThemeProvider>
         <Header />
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
