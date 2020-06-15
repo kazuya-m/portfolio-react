db490d428766ff9dd8d9ed4c86e99a0defd61ee8
kazuya-m <kazuya.matsuo5@gmail.com>
2020-06-15 21:14:23 +0900

    add tab icons

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Skills from './Skills';
 import Profile from './Profile';
+
 import {BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import Icon from '@material-ui/core/Icon';
 
 class Aboutme extends React.Component {
   render() {
@@ -36,28 +38,14 @@ class Aboutme extends React.Component {
             <Tabs
               // value={value}
               // onChange={handleChange}
-              indicatorColor="primary"
-              textColor="primary"
+              // indicatorColor="action"
+//              textColor="primary"
               centered
+              style={style}
             >
-              <Tab label="PROFILE" />
-              <Tab label="SKILLS" />
+              <NavLink to='/Aboutme'><Tab label="PROFILE" icon={<Icon>how_to_reg</Icon>}/></NavLink>
+              <NavLink to='/Aboutme/Skills'><Tab label="SKILLS" icon={<Icon>playlist_add_check</Icon>}/></NavLink>
             </Tabs>
-            <div className="profile-area">
-              <ul id="aboutme-tab">
-                <li
-                  // onClick={() => {this.handleClickedTab("profile")}} 
-                  // className={profileActive}
-                  >
-                  <NavLink to='/Aboutme' activeClassName="active">Profile</NavLink>
-                </li>
-                <li 
-                  // onClick={() => {this.handleClickedTab("skills")}} 
-                  // className={skillsActive}
-                  >
-                  <NavLink to='/Aboutme/Skills' activeClassName="active">Skills</NavLink>
-                </li>
-              </ul>
               <div className="content-area">
                 <Route exact path='/Aboutme' component={Profile} />
                 <Route
@@ -67,10 +55,13 @@ class Aboutme extends React.Component {
               </div>
             </div>
           </div>
-        </div>
+        
       </Router>
     )
   }
 }
 
+const style = {
+  indicatorColor: '#f7f7f7'
+}
 export default Aboutme;
